ce51deea5cafe1304b8a81dddf2e02d6e8a27610
Intuitive Harmony <j.horst77@gmail.com>
2024-04-05 09:43:55 -0600

    initial variables

---
+++ app.js
@@ -0,0 +1,36 @@
+// ~~~~~~~~~~~~~~~~~~
+// Shoshana Variables
+// ~~~~~~~~~~~~~~~~~~
+const numIntroSession = 3;
+let numKAPSession;
+
+// Sliding scale for Shoshana
+const shan = {
+  low: 160,
+  high: 200,
+};
+
+// ~~~~~~~~~~~~~~~~~~~~~
+// Progressive Variables
+// ~~~~~~~~~~~~~~~~~~~~~
+const iv = {
+  initialLow: 475,
+  initialHigh: 550,
+  subsequent: 375,
+};
+
+const im = {
+  initialLow: 375,
+  initialHigh: 450,
+  subsequent: 275,
+};
+
+const oral = {
+  initial: 350,
+  room: {
+    low: 0,
+    high: 50,
+  },
+  medsLow: 65,
+  medsHigh: 85,
+};
