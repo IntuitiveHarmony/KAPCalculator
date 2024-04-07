9fd9d96923aed9c87d7ac4615fd0747d73e5f894
Intuitive Harmony <j.horst77@gmail.com>
2024-04-06 23:20:06 -0600

    Calculate IV

--- app.js
+++ app.js
@@ -54,16 +54,28 @@ const preWorkHigh = document.getElementById("pre-work-high");
 // Medicine type
 const radioButtons = document.querySelectorAll(".medicine-radio");
 const medicineControls = document.querySelectorAll(".medicine-controls");
+
 // KAP Sliders
+// IM Sliders
 const imSessionSlider = document.getElementById("im-sessions-input");
 const imIntegrationSlider = document.getElementById("im-integration-input");
-const imValue = document.getElementById("im-sessions-value");
+const imSessionValue = document.getElementById("im-sessions-value");
 const imIntegrationValue = document.getElementById("im-integration-value");
 const imSessionLow = document.getElementById("im-sessions-low");
 const imSessionHigh = document.getElementById("im-sessions-high");
 const imSessionAdmin = document.getElementById("im-admin");
 const imIntegrationLow = document.getElementById("im-integration-low");
 const imIntegrationHigh = document.getElementById("im-integration-high");
+// Iv Sliders
+const ivSessionSlider = document.getElementById("iv-sessions-input");
+const ivIntegrationSlider = document.getElementById("iv-integration-input");
+const ivSessionValue = document.getElementById("iv-sessions-value");
+const ivIntegrationValue = document.getElementById("iv-integration-value");
+const ivSessionLow = document.getElementById("iv-sessions-low");
+const ivSessionHigh = document.getElementById("iv-sessions-high");
+const ivSessionAdmin = document.getElementById("iv-admin");
+const ivIntegrationLow = document.getElementById("iv-integration-low");
+const ivIntegrationHigh = document.getElementById("iv-integration-high");
 
 // ~~~~~~~~~
 // Listeners
@@ -109,7 +121,7 @@ imSessionSlider.addEventListener("input", (event) => {
   // Update variable for calculator
   numIMSession = currentValue;
   // Update DOM with new variable amount
-  imValue.textContent = numIMSession;
+  imSessionValue.textContent = numIMSession;
   // Calculate the cost for Shoshana and add to the DOM
   imSessionLow.textContent = calculateWorkCost(numIMSession, shan.low)
     .toString()
@@ -148,6 +160,55 @@ imIntegrationSlider.addEventListener("input", (event) => {
     .toString()
     .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   imIntegrationHigh.textContent = calculateWorkCost(numIMIntegration, shan.high)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+});
+
+ivSessionSlider.addEventListener("input", (event) => {
+  // Get the current value of the slider
+  const currentValue = event.target.value;
+  // Update variable for calculator
+  numIVSession = currentValue;
+  // Update DOM with new variable amount
+  ivSessionValue.textContent = numIVSession;
+  // Calculate the cost for Shoshana and add to the DOM
+  ivSessionLow.textContent = calculateWorkCost(numIVSession, shan.low)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  ivSessionHigh.textContent = calculateWorkCost(numIVSession, shan.high)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  // calculate cost for medicine and update DOM
+  ivSessionAdmin.textContent = (numIVSession * im.subsequent)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  // Update Integration sessions variable and DOM
+  numIVIntegration = numIVSession;
+  ivIntegrationValue.textContent = numIVIntegration;
+  // Calculate integration cost and update DOM
+  ivIntegrationLow.textContent = calculateWorkCost(numIVIntegration, shan.low)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  ivIntegrationHigh.textContent = calculateWorkCost(numIVIntegration, shan.high)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  // Update Integration slider params
+  ivIntegrationSlider.min = numIVSession;
+  ivIntegrationSlider.value = numIVSession;
+  // Enable the Integration slider
+  ivIntegrationSlider.disabled = false;
+});
+
+ivIntegrationSlider.addEventListener("input", (event) => {
+  // Update the variable
+  numIVIntegration = event.target.value;
+  // Update the DOM
+  ivIntegrationValue.textContent = numIVIntegration;
+  // Update the cost
+  ivIntegrationLow.textContent = calculateWorkCost(numIVIntegration, shan.low)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  ivIntegrationHigh.textContent = calculateWorkCost(numIVIntegration, shan.high)
     .toString()
     .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 });
